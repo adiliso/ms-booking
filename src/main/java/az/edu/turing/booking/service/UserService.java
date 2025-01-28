b03d62cb640b3a014719c334a8183f6f58d96330
gurbanli <gurbanli009@gmail.com>
2025-01-28 19:08:47 +0400

    user service updated

--- src/main/java/az/edu/turing/booking/service/UserService.java
+++ src/main/java/az/edu/turing/booking/service/UserService.java
@@ -1,20 +1,22 @@
 package az.edu.turing.booking.service;
 
-import az.edu.turing.booking.exception.NotFoundException;
 import az.edu.turing.booking.model.dto.UserDto;
-import az.edu.turing.booking.model.dto.request.CreateUserRequest;
-import az.edu.turing.booking.model.dto.request.UpdateRoleRequest;
-import az.edu.turing.booking.model.dto.request.UpdateUsernameRequest;
+import az.edu.turing.booking.model.dto.request.UserCreateRequest;
+import az.edu.turing.booking.model.dto.request.UserRoleUpdateRequest;
+import az.edu.turing.booking.model.dto.request.UsernameUpdateRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface UserService {
 
-    UserDto create(CreateUserRequest request);
+    @Transactional
+    UserDto create(UserCreateRequest request);
 
-    UserDto updateUsername(long id, UpdateUsernameRequest request) throws NotFoundException;
+    @Transactional
+    UserDto updateUsername(long id, UsernameUpdateRequest request);
 
-    UserDto updateRole(long id, UpdateRoleRequest request) throws NotFoundException;
+    @Transactional
+    UserDto updateRole(long id, UserRoleUpdateRequest request);
 
     boolean isAdmin(long id);
 }
--- src/main/java/az/edu/turing/booking/service/impl/UserServiceImpl.java
+++ src/main/java/az/edu/turing/booking/service/impl/UserServiceImpl.java
@@ -6,52 +6,47 @@ import az.edu.turing.booking.exception.AlreadyExistsException;
 import az.edu.turing.booking.exception.NotFoundException;
 import az.edu.turing.booking.mapper.UserMapper;
 import az.edu.turing.booking.model.dto.UserDto;
-import az.edu.turing.booking.model.dto.request.CreateUserRequest;
-import az.edu.turing.booking.model.dto.request.UpdateRoleRequest;
-import az.edu.turing.booking.model.dto.request.UpdateUsernameRequest;
+import az.edu.turing.booking.model.dto.request.UserCreateRequest;
+import az.edu.turing.booking.model.dto.request.UserRoleUpdateRequest;
+import az.edu.turing.booking.model.dto.request.UsernameUpdateRequest;
 import az.edu.turing.booking.model.enums.UserRole;
 import az.edu.turing.booking.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
     @Override
-    @Transactional
-    public UserDto create(CreateUserRequest request) {
+    public UserDto create(UserCreateRequest request) {
 
-        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+        if (userRepository.existsByUsername(request.getUsername())) {
             throw new AlreadyExistsException("User already exists with username: " + request.getUsername());
         }
         UserEntity userEntity = userMapper.toEntity(request);
+
         userRepository.save(userEntity);
+
         return userMapper.toDto(userEntity);
     }
 
     @Override
-    @Transactional
-    public UserDto updateUsername(long id, UpdateUsernameRequest request) {
+    public UserDto updateUsername(long id, UsernameUpdateRequest request) {
 
-        UserEntity userEntity = userRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+        UserEntity userEntity = existsUserEntity(id);
         userEntity.setUsername(request.getUsername());
         userRepository.save(userEntity);
         return userMapper.toDto(userEntity);
     }
 
     @Override
-    @Transactional
-    public UserDto updateRole(long id, UpdateRoleRequest request) {
+    public UserDto updateRole(long id, UserRoleUpdateRequest request) {
 
-        UserEntity userEntity = userRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+        UserEntity userEntity = existsUserEntity(id);
         userEntity.setRole(request.getRole());
         userRepository.save(userEntity);
         return userMapper.toDto(userEntity);
@@ -60,8 +55,12 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean isAdmin(long id) {
 
-        UserEntity userEntity = userRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+        UserEntity userEntity = existsUserEntity(id);
         return userEntity.getRole() == UserRole.ADMIN;
+    }
+
+    public UserEntity existsUserEntity(long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User not found with id: " + id));
     }
 }
