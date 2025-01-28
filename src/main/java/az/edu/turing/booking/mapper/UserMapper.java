091a6c92a0f1dc8db9e1582295c86752ec4b9f68
gurbanli <gurbanli009@gmail.com>
2025-01-28 19:08:47 +0400

    user service updated

--- src/main/java/az/edu/turing/booking/mapper/UserMapper.java
+++ src/main/java/az/edu/turing/booking/mapper/UserMapper.java
@@ -2,13 +2,13 @@ package az.edu.turing.booking.mapper;
 
 import az.edu.turing.booking.domain.entity.UserEntity;
 import az.edu.turing.booking.model.dto.UserDto;
-import az.edu.turing.booking.model.dto.request.CreateUserRequest;
+import az.edu.turing.booking.model.dto.request.UserCreateRequest;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface UserMapper {
 
-    UserEntity toEntity(CreateUserRequest createUserRequest);
+    UserEntity toEntity(UserCreateRequest userCreateRequest);
 
     UserDto toDto(UserEntity userEntity);
 }
