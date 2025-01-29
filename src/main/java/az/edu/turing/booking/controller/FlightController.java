98b4a3ebbc49d21167ab8354a7d81f6e62d59ba9
Adil İsmayılov <ismayilovadil670@gmail.com>
2025-01-29 23:30:21 +0400

    FlightController updated

--- src/main/java/az/edu/turing/booking/controller/FlightController.java
+++ src/main/java/az/edu/turing/booking/controller/FlightController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v2/flights")
+@RequestMapping("/api/v1/flights")
 public class FlightController {
 
     private final FlightService flightService;
