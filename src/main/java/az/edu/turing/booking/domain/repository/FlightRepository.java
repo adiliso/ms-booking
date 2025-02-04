7d1daf3e9af563acdbdbb983b77572ddfebc6352
Adil İsmayılov <ismayilovadil670@gmail.com>
2025-02-04 16:19:17 +0400

    unused imports deleted

--- src/main/java/az/edu/turing/booking/domain/repository/FlightRepository.java
+++ src/main/java/az/edu/turing/booking/domain/repository/FlightRepository.java
@@ -2,10 +2,8 @@ package az.edu.turing.booking.domain.repository;
 
 import az.edu.turing.booking.domain.entity.FlightEntity;
 import az.edu.turing.booking.model.enums.FlightStatus;
-import jakarta.annotation.Nullable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
